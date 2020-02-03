5e8edc517288202afcc1de73b80a8a307892c314
akimegane0730 <akimegane0730o@gmail.com>
2020-02-04 08:46:07 +0900

    card create
    カード作成機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "tops#index"
 
-  resources :lists, only: [:new, :create, :edit, :update, :destroy]
-  resources :users, only: [:edit, :update]
+  resources :lists, only: [:new, :create, :edit, :update, :destroy] do
+    resources :cards, only: [:new, :create]
+  end
+  resources :users, only: [:edit, :update] 
+  
 end
