e747afd2c65d2a04159d665d2ee40e98067a25c9
akimegane0730 <akimegane0730o@gmail.com>
2020-02-04 11:59:29 +0900

    delete card function
    作ったカードを削除する機能の実装

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,8 +1,11 @@
 class CardsController < ApplicationController
+
+  # before_action set_card:, only: [:show, :edit, :update, :destroy]
   def new
     @card = Card.new
-    # @list = List.find_by(id: params[:list_id])
-    @list = List.find(params[:id])
+    # idの値が不明だからfind_byにしている
+    @list = List.find_by(id: params[:list_id])
+    # @list = List.find(params[:id])
   end
 
   def create
@@ -29,6 +32,12 @@ class CardsController < ApplicationController
     else
       render action :edit
     end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to :root
   end
 
   private
