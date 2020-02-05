e19958c8bb7e434cf9aee695510f11fde8848d87
akimegane0730 <akimegane0730o@gmail.com>
2020-02-05 21:12:32 +0900

    bacjground
    背景に画像を挿入

--- app/controllers/oures_controller.rb
+++ app/controllers/oures_controller.rb
@@ -1,6 +1,10 @@
 class OuresController < ApplicationController
 
   def index
-    @random = Card.order("RAND()").limit(5)
+    @randomA = Card.order("RAND()").limit(1)
+    @randomB = Card.order("RAND()").limit(1)
+    @randomC = Card.order("RAND()").limit(1)
+    @randomD = Card.order("RAND()").limit(1)
+    @randomE = Card.order("RAND()").limit(1)
   end
 end
