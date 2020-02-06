b317e2cbb740eb0d6614902005947d1e1ab0a3b1
akimegane0730 <akimegane0730o@gmail.com>
2020-02-06 14:54:27 +0900

    set destroy action for likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,7 @@
 class LikesController < ApplicationController
 
+  before_action :authenticate_user!
+  
   def create
     @like = current_user.likes.build(like_params)
     @card = @like.card
@@ -7,7 +9,8 @@ class LikesController < ApplicationController
 
 
   def destroy
-    
+    @like = Like.find(params[:id])
+    @card = @like.card
   end
 
   private
