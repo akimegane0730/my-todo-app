e20463c65fb16d0c6af1c7f57d84d3684aba8637
akimegane0730 <akimegane0730o@gmail.com>
2020-02-06 14:41:46 +0900

    set likes controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,14 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  resources :lists, only: [:new, :create, :edit, :update, :destroy] do
+    resources :cards, except: [:index] do
+      resources :likes, only: [:create, :destroy]
+    end
+  end
+
+
   resources :users, only: [:edit, :update, :show] 
 
   resources :oures, only: [:index]
