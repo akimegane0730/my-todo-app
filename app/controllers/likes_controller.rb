0785ea8a101b243537089660c8c71827f8312736
akimegane0730 <akimegane0730o@gmail.com>
2020-02-06 14:52:46 +0900

    set create action for likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,16 @@
 class LikesController < ApplicationController
 
   def create
+    @like = current_user.likes.build(like_params)
+    @card = @like.card
   end
 
 
   def destroy
+    
   end
+
+  private
+    def like_params
+      params.permit(:card_id)
 end
